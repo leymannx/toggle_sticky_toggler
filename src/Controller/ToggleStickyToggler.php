4d23b941470ca2f261ad231be401ce4ea97ad96d
Norman Kämper-Leymann <norman.leymann@gmail.com>
2019-03-11 11:05:48 +0100

    Add missing docs and comments.

--- src/Controller/ToggleStickyToggler.php
+++ src/Controller/ToggleStickyToggler.php
@@ -6,12 +6,20 @@ use Drupal\node\NodeInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Drupal\Core\Url;
 
+/**
+ * Toggle Sticky Toggler controller.
+ */
 class ToggleStickyToggler {
 
   /**
+   * Toggle a node's sticky status.
+   *
    * @param \Drupal\node\NodeInterface $node
+   *   The requested node object.
    *
    * @return \Symfony\Component\HttpFoundation\RedirectResponse
+   *   Redirect response back to URL where the link was clicked.
+   *
    * @throws \Drupal\Core\Entity\EntityStorageException
    */
   public function toggleStickyStatus(NodeInterface $node) {
